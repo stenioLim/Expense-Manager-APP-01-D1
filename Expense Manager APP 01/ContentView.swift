3d770c0e1a052668b4f27d57864a20e5e694b89a
stenioLim <stenio_bomfim@hotmail.com>
2024-06-09 12:32:46 -0300

    Initial Commit

---
+++ Expense Manager APP 01/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Expense Manager APP 01
+//
+//  Created by stenio Lima on 09/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Expense Manager APP 01/Expense_Manager_APP_01App.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_Manager_APP_01App.swift
+//  Expense Manager APP 01
+//
+//  Created by stenio Lima on 09/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Expense_Manager_APP_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
